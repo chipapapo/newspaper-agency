d31e3cf11d3d04d6e4446d16b36a9b87ba159eec
chipa <mezismezis@gmail.com>
2023-07-06 15:26:29 +0300

    Created main page, created index.html, login.html, register.html, updated views.py

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from agency.models import (
+    Topic,
+    Redactor,
+    Newspaper
+)
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_topics = Topic.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_topics": num_topics,
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "pages/index.html", context=context)
