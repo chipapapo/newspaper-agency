8e396c1c85d1c7eade24aeb4dcbaa49824bca7a9
chipa <mezismezis@gmail.com>
2023-07-20 11:40:51 +0300

    Fixed redirecting from login to profile problem, setting.py

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+LOGIN_REDIRECT_URL = ""
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
