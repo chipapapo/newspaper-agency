2b7808f5647e876d54ad9d5c00ec99c7950f0663
chipa <mezismezis@gmail.com>
2023-07-20 12:02:18 +0300

    Small changes
    Expanded .gitignore, addded some advices in README.md, fixed login redirect, added bootstrap to requirements.txt

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-LOGIN_REDIRECT_URL = ""
+LOGIN_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
