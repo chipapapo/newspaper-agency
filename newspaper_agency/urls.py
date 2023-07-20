0c27f2a5898291dbb88dd9d42e6466ea44458ef4
chipa <mezismezis@gmail.com>
2023-07-20 11:39:33 +0300

    Added urls for soft ui features/pages

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -18,9 +18,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from agency import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("agency.urls", namespace="agency")),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/login/', views.UserLoginView.as_view(), name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('accounts/register/', views.register, name='register'),
+    path('accounts/password-change/', views.UserPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password-change-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name="password_change_done"),
+    path('accounts/password-reset/', views.UserPasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password-reset-confirm/<uidb64>/<token>/',
+        views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('accounts/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
