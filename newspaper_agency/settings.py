7cb6e77eb8c54bcdce336e6e908c43339ba7ca5e
chipa <mezismezis@gmail.com>
2023-07-24 13:59:38 +0300

    Fixed setting.py, deployment decision

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO-SECRET-KEY",
-    "django-insecure-ahm&(@=+&h#lnm3nt4+8u8&g1-)#k+!3sk$sm3e9oqf%*r_=s9"
-)
+SECRET_KEY = os.environ["DJANGO-SECRET-KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
